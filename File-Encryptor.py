3f87650d627790ad8fe3a44fc3b9bf6069293651
Liam <138702588+Totodile-Liam@users.noreply.github.com>
2023-09-11 20:24:51 +0800

    Update File-Encryptor.py

--- File-Encryptor.py
+++ File-Encryptor.py
@@ -1,77 +1,96 @@
-#Imports
-
 import os
+import sys
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
-AES_blocksize = 16
-AES_padding = b'\x00'
-
-#RSA Key Pair
-key = RSA.generate(2048) 
-private_key = key.export_key()
-public_key = key.publickey().export_key()
-
-# RSA Key write to file
-with open('private_key.pem', 'wb') as f:
-    f.write(private_key)
-with open('public_key.pem', 'wb') as f:
-    f.write(public_key)
+AES_KEY_LENGTH = 32  # 256 bits
+RSA_KEY_LENGTH = 2048
 
 def generate_aes_key():
-    #Generate a random AES key.
-    return get_random_bytes(AES_blocksize)
+    return get_random_bytes(AES_KEY_LENGTH)
 
-def encrypt_file(filename, aes_key, rsa_public_key):
-    #Encrypt a file using AES and RSA.
-    #Read file contents
-    with open(filename, 'rb') as file:
-        plaintext = file.read()
+def encrypt_file(input_filename, output_filename, aes_key, rsa_public_key):
+    try:
+        with open(input_filename, 'rb') as file:
+            plaintext = file.read()
 
-    #Pad the plaintext to be a multiple of AES block size
-    plaintext += AES_padding * (AES_blocksize - len(plaintext) % AES_blocksize)
+        # Pad the plaintext to be a multiple of AES block size
+        padded_plaintext = pad(plaintext, AES.block_size)
 
-    #Generate AES cipher using the AES key
-    aes_cipher = AES.new(aes_key, AES.MODE_EAX)
+        # Generate AES cipher using the AES key
+        aes_cipher = AES.new(aes_key, AES.MODE_EAX)
 
-    #Encrypt the plaintext using AES
-    ciphertext, tag = aes_cipher.encrypt_and_digest(plaintext)
+        # Encrypt the plaintext using AES
+        ciphertext, tag = aes_cipher.encrypt_and_digest(padded_plaintext)
 
-    #Encrypt the AES key using RSA public key
-    rsa_cipher = PKCS1_OAEP.new(rsa_public_key)
-    encrypted_aes_key = rsa_cipher.encrypt(aes_key)
-    
-    #Writing encrypted AES key and ciphertext to output file
-    with open('encrypted_' + filename, 'wb') as output_file:
-        output_file.write(encrypted_aes_key)
-        output_file.write(aes_cipher.nonce)
-        output_file.write(tag)
-        output_file.write(ciphertext) 
+        # Encrypt the AES key using RSA public key
+        rsa_cipher = PKCS1_OAEP.new(rsa_public_key)
+        encrypted_aes_key = rsa_cipher.encrypt(aes_key)
 
-def decrypt_file(filename, aes_key, rsa_private_key):
-    #Decrypt a file using AES and RSA private key
-    rsa_cipher = PKCS1_OAEP.new(rsa_private_key)
-    decrypted_aes_key = rsa_cipher.decrypt(aes_key)
+        # Write encrypted AES key, nonce, tag, and ciphertext to the output file
+        with open(output_filename, 'wb') as output_file:
+            output_file.write(encrypted_aes_key)
+            output_file.write(aes_cipher.nonce)
+            output_file.write(tag)
+            output_file.write(ciphertext)
 
-    # Read encrypted file contents
-    with open(filename, 'rb') as file:
-        encrypted_data = file.read()
-    
-    # Extract AES cipher nonce, tag, and ciphertext from encrypted data
-    encrypted_aes_key_size = rsa_private_key.size_in_bytes()
-    nonce = encrypted_data[:AES_blocksize]
-    tag = encrypted_data[AES_blocksize:AES_blocksize+16]
-    ciphertext = encrypted_data[AES_blocksize+16:encrypted_aes_key_size]
+        print(f"Encryption complete. Encrypted file saved as '{output_filename}'")
 
-    # Create AES cipher using decrypted AES key and extracted nonce
-    aes_cipher = AES.new(decrypted_aes_key, AES.MODE_EAX, nonce=nonce)
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
-    # Decrypting the ciphertext using AES
-    plaintext = aes_cipher.decrypt_and_verify(ciphertext, tag)
+def decrypt_file(input_filename, output_filename, rsa_private_key):
+    try:
+        # Read encrypted file contents
+        with open(input_filename, 'rb') as file:
+            encrypted_data = file.read()
 
-    # Writing the decrypted plaintext to output file
-    with open('decrypted_' + filename, 'wb') as output_file:
-        output_file.write(plaintext)
+        # Extract AES cipher nonce, tag, and ciphertext from encrypted data
+        encrypted_aes_key_size = rsa_private_key.size_in_bytes()
+        nonce = encrypted_data[:AES.block_size]
+        tag = encrypted_data[AES.block_size:AES.block_size + 16]
+        ciphertext = encrypted_data[AES.block_size + 16:encrypted_aes_key_size]
 
+        # Decrypt the AES key using RSA private key
+        rsa_cipher = PKCS1_OAEP.new(rsa_private_key)
+        aes_key = rsa_cipher.decrypt(encrypted_data[:RSA_KEY_LENGTH // 8])
 
+        # Create AES cipher using decrypted AES key and extracted nonce
+        aes_cipher = AES.new(aes_key, AES.MODE_EAX, nonce=nonce)
+
+        # Decrypt the ciphertext using AES
+        decrypted_data = unpad(aes_cipher.decrypt(ciphertext), AES.block_size)
+
+        # Write the decrypted plaintext to the output file
+        with open(output_filename, 'wb') as output_file:
+            output_file.write(decrypted_data)
+
+        print(f"Decryption complete. Decrypted file saved as '{output_filename}'")
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: python file_encrypt_decrypt.py <encrypt/decrypt> <input_file> <output_file> <private_key_file>")
+        sys.exit(1)
+
+    operation = sys.argv[1]
+    input_filename = sys.argv[2]
+    output_filename = sys.argv[3]
+    private_key_filename = sys.argv[4]
+
+    if operation == "encrypt":
+        aes_key = generate_aes_key()
+        rsa_private_key = RSA.import_key(open(private_key_filename).read())
+        encrypt_file(input_filename, output_filename, aes_key, rsa_private_key)
+    elif operation == "decrypt":
+        rsa_private_key = RSA.import_key(open(private_key_filename).read())
+        decrypt_file(input_filename, output_filename, rsa_private_key)
+    else:
+        print("Invalid operation. Use 'encrypt' or 'decrypt'.")
+
+if __name__ == "__main__":
+    main()
